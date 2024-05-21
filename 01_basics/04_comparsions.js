bcf2b31c7cb34aeafd3fa7a1899c727482078cf1
PrashantXKratos <142013013+prashantXK@users.noreply.github.com>
2024-05-21 05:36:46 +0000

    dataType summary

---
+++ 01_basics/04_comparsions.js
@@ -0,0 +1,16 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null == 0)    // equality check and less than equal to are different in js
+console.log(null>0); 
+console.log(null>=0);   
+console.log(undefined == 0);
+
+
+
+
+//=== strict check
+
+console.log("2" == 2)
+console.log("2" === 2);
+console.log(0 === "0")
---
+++ 01_basics/05_dataTypes_Summary.js
@@ -0,0 +1,47 @@
+/*Primitve Data Types 
+
+--> String, Number, Boolean, BigInt, Null, Undefined, Symbol
+*/
+
+const score = 100
+const scoreValue = 100.8
+
+const isLoggesIn = false
+const outsideTemp = null
+
+let userEmail;
+
+//symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+//BigInt
+const bigNumber = 9348982348308202239743242n
+
+
+
+// Refrence Data Types (Non Primitve)
+
+// -> Arrays, Object, Functions
+
+const heroes = ["IronMan", "Batman" , "Superman"]
+
+//objects
+let myObj = {
+
+    name:"Prashant",
+    age:20,
+
+}
+
+//functions
+
+const myFunction= function(){
+    console.log("Hello");
+}
+myFunction()
+
+console.log(typeof BigInt)
+
