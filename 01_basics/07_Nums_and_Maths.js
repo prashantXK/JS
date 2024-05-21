a7d591a8ae1c8432bf32b1afce6d44fe0682161c
PrashantXKratos <142013013+prashantXK@users.noreply.github.com>
2024-05-21 17:52:30 +0000

    nums and maths

---
+++ 01_basics/07_Nums_and_Maths.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2))
+
+
+const otherNumber = 612.53436
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++MATHS++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(4))  //absolute value -> negative to positive
+// console.log(Math.round(6.4)) //round off
+// console.log(Math.ceil(6.4))// top value
+// console.log(Math.floor(6.4))// lowest value
+
+// console.log(Math.min(4,6,1,0,9,7));
+// console.log(Math.max(9,6,3,2,1,0,5,19))
+
+// console.log(Math.random());  //random values between 0 and 1
+// console.log((Math.random()*10) + 1); 
+// console.log((Math.random()*10) + 1); 
+
+console.log((Math.random()*10) + 1); 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()  *  (max - min + 1))+ min)
