eb8f14495e61a60839c554d2a99c40d9ff6660cf
PrashantXKratos <142013013+prashantXK@users.noreply.github.com>
2024-05-25 17:23:29 +0000

    ify

---
+++ 03_basics/04_ify.js
@@ -0,0 +1,10 @@
+// immediately invoked functions (IIFE)
+(function tea(){
+    console.log(`db connected`)   //function wrapped in parenthesis , problem from global scope pollution 
+})()
+
+
+// (  function coffee() {
+//     console.log(`db connected too`)
+// } )()
+
