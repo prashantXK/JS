900aeb2fcf4fd15a93392047f75ed386af8be72d
PrashantXKratos <142013013+prashantXK@users.noreply.github.com>
2024-05-25 08:22:37 +0000

    scopes/mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,13 +11,49 @@ if (true) {
     let x = 10
     const y = 20
     // var z = 30
-    console.log("Inner scope: ",x)
+    // console.log("Inner scope: ",x)
 }
 
-console.log("Outside scope: ",x);  //error
+// console.log("Outside scope: ",x);  //error
 // console.log(y);  //error
 // console.log(z);     //30
 
-// {}  // --curly braces with functions / statements are the scope
+// {}  // --curly braces with functions and statements are the scope
 
+//---------NESTED SCOPES-----------
 
+function one() {
+    const username = "kratos"
+
+    function two() {
+        const website = "instagram"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+// one()
+
+if (true) {
+    const username = "kratos"
+
+    if (username === "kratos") {
+        const website = " youtube"
+        // console.log(username+ website);
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+//++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++
+console.log(addOne(5))
+function addOne(num) {
+    return num + 1
+}
+
+addTwo(5)
+const addTwo
+    = function (num) {
+        return num + 2
+    }
